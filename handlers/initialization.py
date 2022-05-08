905dae610bfb7dfa77dc173dbcdd8884b073273e
Atmoslayer <ddsomdim@gmail.com>
2022-05-08 15:06:11 +0300

    Сделано ветвление с проверкой регистрации пользователя

--- handlers/initialization.py
+++ handlers/initialization.py
@@ -3,6 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from create_bot import dispatcher, bot
+from functions import get_user_data
 
 user_info = {'first_name': '',
              'last_name': '',
@@ -12,7 +13,11 @@ user_info = {'first_name': '',
 
 menu = ''
 
+username = ''
+
+
 class Initialization(StatesGroup):
+    waiting_for_check_user = State()
     waiting_for_enter_name = State()
     waiting_for_confirm_name = State()
     waiting_for_enter_contact = State()
@@ -20,8 +25,28 @@ class Initialization(StatesGroup):
 
 
 @dispatcher.message_handler(commands=['start'])
+# Здесь стартует приём сообщений от пользователя
+async def check_user(message: types.Message, state: FSMContext):
+    user_id = message.from_user.username
+    user_info = get_user_data(user_id)  #Здесь вызывается функция для получения данных о пользователе
+    if user_info: #Если данные есть, говорим, что они есть и сразу переходим к процессу сборки торта
+        button_start_order = KeyboardButton('Собрать торт')
+        button_check_orders = KeyboardButton('История заказов')
+        global menu
+        menu = ReplyKeyboardMarkup(resize_keyboard=True)
+        menu.add(button_start_order, button_check_orders)
+        await bot.send_message(message.from_user.id, text='Вы уже зарегистрированы', reply_markup=menu)
+        print(user_info)
+        await state.finish()
+    else: #Если данных нет, идём по процессу регистрации
+        await Initialization.waiting_for_check_user.set()
+
+
+@dispatcher.message_handler(state=Initialization.waiting_for_check_user)
 async def get_name(message: types.Message):
     global user_info
+    global username
+    username = message.from_user.username
     user_info['first_name'] = message.from_user.first_name
     user_info['last_name'] = message.from_user.last_name
     user_info['username'] = message.from_user.username
@@ -85,14 +110,14 @@ async def confirm_contact(message: types.Message, state: FSMContext):
 
     await bot.send_message(message.from_user.id, text='Спасибо, регистрация пройдена', reply_markup=menu)
 
-    print(user_info)
+    print(user_info) #К этому моменту собрана вся информация о пользователе, здесь в БД можно сохранять всё, что касается регистрации 
     await state.finish()
 
 
-# def register_handlers_food(dp: dispatcher):
-#     dp.register_message_handler(get_name, commands='registration', state="*")
-#     dp.register_message_handler(confirm_name, state=Initialization.waiting_for_enter_name)
-#     dp.register_message_handler(ask_name, state=Initialization.waiting_for_confirm_name)
-#     dp.register_message_handler(read_name, state=Initialization.waiting_for_enter_name)
-#     dp.register_message_handler(get_contact, state=Initialization.waiting_for_enter_contact)
-#     dp.register_message_handler(confirm_contact, state=Initialization.waiting_for_confirm_contact)
+# defegister_handlers_food(dp: dispatcher):
+# #     dp.register_message_handler(get_name, commands='registration', state="*")
+# #     dp.register_message_handler(confirm_name, state=Initialization.waiting_for_enter_name)
+# #     dp.register_message_handler(ask_name, state=Initialization.waiting_for_confirm_name)
+# #     dp.register_message_handler(read_name, state=Initialization.waiting_for_enter_name)
+# #     dp.register_message_handler(get_contact, state=Initialization.waiting_for_enter_contact)
+# #     dp.register_message_handler(confirm_contact, state=Initialization.waiting_for_confirm_contact) r
